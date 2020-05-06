3b7d464425b677c8752d1f18061a0d1b72dde4c3
Diego Schvartzman <diego@holaemi.com>
2020-05-06 19:59:00 -0300

    edit mode refactored out of flow management bar

--- src/examples/emi-flow-editor/components/flow-management-bar/flow-management-bar.js
+++ src/examples/emi-flow-editor/components/flow-management-bar/flow-management-bar.js
@@ -15,6 +15,7 @@ import NameInput from './name-input';
 import RestoreButton from './restore-button';
 import NewButton from './new-button';
 import CloneButton from './clone-button';
+import EditModeButton from './edit-mode-button';
 
 class FlowManagementBar extends React.Component {
   constructor(props) {
@@ -292,21 +293,14 @@ class FlowManagementBar extends React.Component {
     return flowName && !legacy && flowEnv === STG && !flowVersionId;
   };
 
-  editModeClasses = () =>
-    GraphUtils.classNames(
-      ['managerButton']
-        .concat(this.editModeEnabled() ? ['enabled'] : [])
-        .concat(this.state.editMode ? ['editMode'] : [])
-    );
-
   switchEditMode = () => {
     if (!this.editModeEnabled()) {
       return;
     }
 
-    this.setState(prevState =>
-      Object.assign(prevState, { editMode: !prevState.editMode })
-    );
+    this.setState(prevState => ({
+      editMode: !prevState.editMode,
+    }));
   };
 
   deleteEnabled = () => {
@@ -358,7 +352,13 @@ class FlowManagementBar extends React.Component {
   versionsEnabled = () => this.state.s3stored && this.props.flowName;
 
   render() {
-    const { flowEnv, legacy, saving, versionLastModified } = this.state;
+    const {
+      editMode,
+      flowEnv,
+      legacy,
+      saving,
+      versionLastModified,
+    } = this.state;
     const {
       s3Available,
       flowManagementHandlers: {
@@ -387,23 +387,11 @@ class FlowManagementBar extends React.Component {
               getFlows={getFlows}
               unsavedChanges={this.unsavedChanges}
             />
-            <Tooltip content="Edit" distance={5} padding="6px">
-              <svg
-                id="editFlowBtn"
-                className={this.editModeClasses()}
-                style={{
-                  enableBackground: 'new 0 0 24 24',
-                }}
-                height="24px"
-                version="1.1"
-                viewBox="0 0 24 24"
-                width="24px"
-                xmlns="http://www.w3.org/2000/svg"
-                onClick={() => this.switchEditMode()}
-              >
-                <path d="M21.635,6.366c-0.467-0.772-1.043-1.528-1.748-2.229c-0.713-0.708-1.482-1.288-2.269-1.754L19,1C19,1,21,1,22,2S23,5,23,5  L21.635,6.366z M10,18H6v-4l0.48-0.48c0.813,0.385,1.621,0.926,2.348,1.652c0.728,0.729,1.268,1.535,1.652,2.348L10,18z M20.48,7.52  l-8.846,8.845c-0.467-0.771-1.043-1.529-1.748-2.229c-0.712-0.709-1.482-1.288-2.269-1.754L16.48,3.52  c0.813,0.383,1.621,0.924,2.348,1.651C19.557,5.899,20.097,6.707,20.48,7.52z M4,4v16h16v-7l3-3.038V21c0,1.105-0.896,2-2,2H3  c-1.104,0-2-0.895-2-2V3c0-1.104,0.896-2,2-2h11.01l-3.001,3H4z" />
-              </svg>
-            </Tooltip>
+            <EditModeButton
+              enabled={this.editModeEnabled()}
+              active={editMode}
+              switchEditMode={this.switchEditMode}
+            />
             <Tooltip content="Save" distance={5} padding="6px">
               <svg
                 id="saveFlowBtn"
